8f3c89af44b3cd5530d1c216776361ba5234d483
git <gitsilence@gmail.com>
2025-12-07 20:32:05 +0800

    fix format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['pdf-parse', '@napi-rs/canvas'],
+  },
 };
 
 module.exports = nextConfig;
